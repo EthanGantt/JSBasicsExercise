f6b5f9869d99fbd271363f5de5a485c5f1c5489c
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-01-12 09:55:16 -0600

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(21);
+// Numbers, Strings, Booleans, Null, & Undefined
+// +, -, *, /, **, %
+console.log(5 % 4);
+console.log(5 % 2);
+console.log('I\'m Mary Poppins, Y \'all ');
+console.log("Hawk" + "Eye");
+console.log("Ethan Gantt".length);
+console.log("Avengers Endgame"[15]);
+console.log("/");
+console.log(5 ** 4 - 9);
