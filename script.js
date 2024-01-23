aa78ba2796952116d94277f86f96de88d8a6f430
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-01-23 10:36:33 -0600

    Finished

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ console.log('I\'m Mary Poppins, Y \'all ');
 console.log("Hawk" + "Eye");
 console.log("Ethan".length + "Gantt".length);
 console.log("Avengers: Endgame"[16]);
-console.log("/");
+console.log("\\");
 console.log(5 ** 4 - 9);
